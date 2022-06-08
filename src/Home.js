82bc83d759f94e94cdb1af74acf8307b8b27b324
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2022-06-08 13:10:02 +0000

    convert class components to functional component

--- src/Home.js
+++ src/Home.js
@@ -1,46 +1,39 @@
-import React from "react";
+import { useState } from "react";
 import firebase from "./Firebase";
-
 import ToDoItem from "./ToDoItem";
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      item: "",
-    };
+function Home({ user, toDoList, addToDo, userID }) {
+  const [state, setState] = useState({
+    item: "",
+  });
 
-    this.handleChange = this.handleChange.bind(this);
-    this.addItem = this.addItem.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
-  }
-
-  handleChange(e) {
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    setState({ ...state, [name]: value });
   }
 
-  addItem(e) {
+  const addItem = (e) => {
     e.preventDefault();
-    this.props.addToDo(this.state.item);
-    this.setState({
+    addToDo(state.item);
+    setState({
+      ...state,
       item: "",
     });
   }
 
-  deleteItem(e, id) {
+  const deleteItem = (e, id) => {
     e.preventDefault();
-    const ref = firebase.database().ref(`ToDo/${this.props.userID}/${id}`);
+    const ref = firebase.database().ref(`ToDo/${userID}/${id}`);
     ref.remove();
   }
 
   // if use arrow function, do not need to bind in the constructor
-  handleChecked = (e, id) => {
+  const handleChecked = (e, id) => {
     e.preventDefault();
-    const ref = firebase.database().ref(`ToDo/${this.props.userID}/${id}`);
+    const ref = firebase.database().ref(`ToDo/${userID}/${id}`);
     const date = new Date();
     const lastUpdated = date.getTime();
-    let updatedItem = this.props.toDoList.filter((item) => {
+    let updatedItem = toDoList.filter((item) => {
       return item.id === id;
     });
 
@@ -63,88 +56,84 @@ class Home extends React.Component {
     //   });
   };
 
-  render() {
-    const { user, toDoList } = this.props;
+  const unCheckedList = toDoList.filter((item) => {
+    return item.completed === false;
+  });
 
-    const unCheckedList = toDoList.filter((item) => {
-      return item.completed === false;
-    });
+  const checkedList = toDoList.filter((item) => {
+    return item.completed === true;
+  });
 
-    const checkedList = toDoList.filter((item) => {
-      return item.completed === true;
-    });
-
-    return (
-      <div className="container">
-        <h1 className="text-center">Welcome {user}</h1>
-        <form className="row justify-content-center" onSubmit={this.addItem}>
-          <div className="card bg-light col-lg-8">
-            <div className="card-body">
-              <section className="col-sm-12 form-group">
-                <label htmlFor="item" className="form-control-label">
-                  Add new task :
-                </label>
-                <input
-                  type="text"
-                  name="item"
-                  id="item"
-                  required
-                  placeholder="What do you want to accomplish?"
-                  value={this.state.item}
-                  onChange={this.handleChange}
-                  className="form-control"
-                ></input>
-                <div className="form-group text-right mt-3">
-                  <button
-                    type="submit"
-                    className="btn btn-sm btn-info"
-                    id="buttonAdd"
-                  >
-                    + Add task
-                  </button>
-                </div>
-              </section>
-            </div>
+  return (
+    <div className="container">
+      <h1 className="text-center">Welcome {user}</h1>
+      <form className="row justify-content-center" onSubmit={addItem}>
+        <div className="card bg-light col-lg-8">
+          <div className="card-body">
+            <section className="col-sm-12 form-group">
+              <label htmlFor="item" className="form-control-label">
+                Add new task :
+              </label>
+              <input
+                type="text"
+                name="item"
+                id="item"
+                required
+                placeholder="What do you want to accomplish?"
+                value={state.item}
+                onChange={handleChange}
+                className="form-control"
+              ></input>
+              <div className="form-group text-right mt-3">
+                <button
+                  type="submit"
+                  className="btn btn-sm btn-info"
+                  id="buttonAdd"
+                >
+                  + Add task
+                </button>
+              </div>
+            </section>
           </div>
-        </form>
+        </div>
+      </form>
 
-        {unCheckedList.length ? (
-          <h3 className="text-center mt-3">
-            Tasks ({unCheckedList.length})
-          </h3>
-        ) : null}
+      {unCheckedList.length ? (
+        <h3 className="text-center mt-3">
+          Tasks ({unCheckedList.length})
+        </h3>
+      ) : null}
 
-        {unCheckedList.map((item) => (
-          <ToDoItem
-            id={item.id}
-            item={item.item}
-            key={item.id} // without key change, checkbox does not toggle
-            // key={Math.random()}
-            completed={item.completed}
-            handleChecked={this.handleChecked}
-            deleteItem={this.deleteItem}
-          />
-        ))}
+      {unCheckedList.map((item) => (
+        <ToDoItem
+          id={item.id}
+          item={item.item}
+          key={item.id} // without key change, checkbox does not toggle
+          // key={Math.random()}
+          completed={item.completed}
+          handleChecked={handleChecked}
+          deleteItem={deleteItem}
+        />
+      ))}
 
-        {checkedList.length ? (
-          <h3 className="text-center mt-3">
-            Completed ({checkedList.length})
-          </h3>
-        ) : null}
-        {checkedList.map((item) => (
-          <ToDoItem
-            id={item.id}
-            item={item.item}
-            key={item.id} // without key change, checkbox does not toggle
-            // key={Math.random()}
-            completed={item.completed}
-            handleChecked={this.handleChecked}
-            deleteItem={this.deleteItem}
-          />
-        ))}
-      </div>
-    );
-  }
+      {checkedList.length ? (
+        <h3 className="text-center mt-3">
+          Completed ({checkedList.length})
+        </h3>
+      ) : null}
+      {checkedList.map((item) => (
+        <ToDoItem
+          id={item.id}
+          item={item.item}
+          key={item.id} // without key change, checkbox does not toggle
+          // key={Math.random()}
+          completed={item.completed}
+          handleChecked={handleChecked}
+          deleteItem={deleteItem}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Home;
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,49 +1,43 @@
-// This is the landing page of this app
-
-import React from "react";
 import { Link } from "@reach/router";
 import { FcTodoList } from "react-icons/fc";
 
-class Navigation extends React.Component {
-  render() {
-    const { user, logOutUser } = this.props;
-    return (
-      <nav className="site-nav family-sans navbar navbar-expand bg-primary navbar-dark higher sticky-top">
-        <div className="container">
-          <div className="navbar-nav ml-auto">
-            <Link to="/" className="navbar-brand">
-              <FcTodoList className="mr-1" /> ReTasks
-            </Link>
-            {user && (
-              <>
-                <Link className="nav-item nav-link" to="/home">
-                  Home
-                </Link>
-                <Link
-                  className="nav-item nav-link"
-                  to="/login"
-                  onClick={(e) => logOutUser(e)}
-                >
-                  log out
-                </Link>
-              </>
-            )}
-            {!user && (
-              <>
-                <Link className="nav-item nav-link" to="/register">
-                  register
-                </Link>
+function Navigation({ user, logOutUser }) {
+  return (
+    <nav className="site-nav family-sans navbar navbar-expand bg-primary navbar-dark higher sticky-top">
+      <div className="container">
+        <div className="navbar-nav ml-auto">
+          <Link to="/" className="navbar-brand">
+            <FcTodoList className="mr-1" /> ReTasks
+          </Link>
+          {user && (
+            <>
+              <Link className="nav-item nav-link" to="/home">
+                Home
+              </Link>
+              <Link
+                className="nav-item nav-link"
+                to="/login"
+                onClick={(e) => logOutUser(e)}
+              >
+                log out
+              </Link>
+            </>
+          )}
+          {!user && (
+            <>
+              <Link className="nav-item nav-link" to="/register">
+                register
+              </Link>
 
-                <Link className="nav-item nav-link" to="/login">
-                  login
-                </Link>
-              </>
-            )}
-          </div>
+              <Link className="nav-item nav-link" to="/login">
+                login
+              </Link>
+            </>
+          )}
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
 
 export default Navigation;
